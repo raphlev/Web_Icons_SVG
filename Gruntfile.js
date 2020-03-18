a5d8ea27bd52b1e7fbd00bcdf7cc295cc4c308f0
Leveque <rleveque@ptc.com>
2020-03-18 16:46:02 +0100

    MAJ

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,59 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-// Project configuration.
-grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    // Optimize SVGs
-    svgmin: {
-        dist: {
-            options: {
-                plugins: [
-                    { removeXMLProcInst: false },       // Don't remove XML declaration (needed to avoid errors creating PNG on Win 7)
-                    { removeViewBox: false },           // Don't remove the viewbox attribute from the SVG
-                    { removeEmptyAttrs: false }         // Don't remove Empty Attributes from the SVG
-                ]
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Optimize SVGs
+        svgmin: {
+            dist: {
+                options: {
+                    plugins: [{
+                            removeXMLProcInst: false
+                        }, // Don't remove XML declaration (needed to avoid errors creating PNG on Win 7)
+                        {
+                            removeViewBox: false
+                        }, // Don't remove the viewbox attribute from the SVG
+                        {
+                            removeEmptyAttrs: false
+                        } // Don't remove Empty Attributes from the SVG
+                    ]
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'icons/socialmedia/SVG_raw',
+                    src: ['*.svg'],
+                    dest: 'icons/socialmedia/SVG_optim',
+                }]
             },
-            files: [{
-                expand: true,
-                cwd: 'icons/socialmedia/SVG_raw',
-                src: ['*.svg'],
-                dest: 'icons/socialmedia/SVG_optim',
-            }]
         },
-    },
-    // Do the Grunticon thing
-    grunticon: {
-        myIcons: {
-            files: [{
-                expand: true,
-                cwd: 'icons/socialmedia/SVG_optim',
-                src: ['*.svg', '*.png'],
-                dest: "icons/socialmedia/SVG"
-            }],
-            options: {
+        // Do the Grunticon thing
+        grunticon: {
+            myIcons: {
+                files: [{
+                    expand: true,
+                    cwd: 'icons/socialmedia/SVG_optim',
+                    src: ['*.svg', '*.png'],
+                    dest: "icons/socialmedia/SVG"
+                }],
+                options: {
+                    enhanceSVG: true
+                }
             }
-        }
-    },
-    watch: {
-        svgs: {
-            files: 'icons/socialmedia/SVG_raw/*.svg',
-            tasks: ['svgmin', 'grunticon'],
-        }
-    },
-});
+        },
+        watch: {
+            svgs: {
+                files: 'icons/socialmedia/SVG_raw/*.svg',
+                tasks: ['svgmin', 'grunticon'],
+            }
+        },
+    });
 
-//require('load-grunt-tasks')(grunt);
-grunt.loadNpmTasks('grunt-svgmin');
-grunt.loadNpmTasks('grunt-grunticon');
-grunt.loadNpmTasks('grunt-contrib-watch');
+    //require('load-grunt-tasks')(grunt);
+    grunt.loadNpmTasks('grunt-svgmin');
+    grunt.loadNpmTasks('grunt-grunticon');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-// Default task(s).
-grunt.registerTask('default', 'watch');
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
 
 };
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# SVG Icons
-Experimental playground for the Lynda.com course
+# grunticon and grumpicon
+
+grunticon is a Grunt.js task that makes it easy to manage icons and background images for all devices, preferring HD (retina) SVG icons but also provides fallback support for standard definition browsers, and old browsers alike. From a CSS perspective, it's easy to use, as it generates a class referencing each icon, and doesn't use CSS sprites.
+
+grunticon takes a folder of SVG/PNG files (typically, icons that you've drawn in an application like Adobe Illustrator), and outputs them to CSS in 3 formats: svg data urls, png data urls, and a third fallback CSS file with references to regular png images, which are also automatically generated and placed in a folder.
+
+grunticon also generates a small bit of JavaScript to drop into your site, which asynchronously loads the appropriate icon CSS depending on a browser's capabilities, and a preview HTML file with that loader script in place.
+
+You can see a demonstration of the output here.
+..
+..
+https://github.com/filamentgroup/grunticon
+
+Drag & Drop ur SVGs on the Grumpicon
+..
+..
+http://www.grumpicon.com/
+
+# Execute this command to avoid Windows execution issue first
+
+Set-ExecutionPolicy Unrestricted
+
+# Install
+
+npm install
+
+# Execute
+
+grunt
+
+# Update a file in /socialmedia/SVG_raw
+
+This should minify all svg files from SVG_raw to SVG_optim folder
+This should copy svg css inline in index.html
+
+https://www.linkedin.com/learning/creating-web-icons-with-svg/display-grunticon-svg-icons-in-html-documents?u=76815018
+Chapter 5.5
